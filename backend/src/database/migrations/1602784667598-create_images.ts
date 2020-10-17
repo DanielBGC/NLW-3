fb1a47e28da4ee5548bbd8176950d1836cfc4399
DanielBGC <daniel.borba-@hotmail.com>
2020-10-17 14:37:50 -0300

    backend folder - API

---
+++ backend/src/database/migrations/1602784667598-create_images.ts
@@ -0,0 +1,44 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createImages1602784667598 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "images",
+            columns: [
+                {
+                    name: "id",
+                    type: "integer",
+                    unsigned: true,
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: "increment",
+                },
+                {
+                    name: "path",
+                    type: "varchar",
+                }, 
+                {
+                    name: "orphanage_id",
+                    type: "integer",
+                }
+            ],
+            foreignKeys: [
+                {
+                    name: "image_orphanage",
+                    columnNames: ["orphanage_id"],
+                    referencedTableName: "orphanages",
+                    //referencedTableName: "orphanage",
+                    referencedColumnNames: ["id"],
+                    onUpdate: "CASCADE",
+                    onDelete: "CASCADE",
+                }
+            ],
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("images");
+    }
+
+}
